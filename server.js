7f0ae23160dbb68950d25d83baf1d5b8161a7758
alvisb <aaalvis.33@gmail.com>
2016-02-16 14:48:22 +0000

    added env.PORT

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
-http.listen(3000, function(){
+http.listen(process.env.PORT || 3000, function(){
   console.log('listening on *:3000');
 });
 
