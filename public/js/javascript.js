d210ac19b90d894a511865f07e01c21908eeb90b
alvisb <aaalvis.33@gmail.com>
2016-02-27 22:08:49 +0000

    sdfsd

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -131,39 +131,46 @@ loader.load('models/station.json', function (geometry) {
   stationMesh.material = material;
 });
 
+var skybox;
 
-var urls = [
-  'img/box_right1.jpg',
-  'img/box_left2.jpg',
-  'img/box_top3.jpg',
-  'img/box_bottom4.jpg',
-  'img/box_front5.jpg',
-  'img/box_back6.jpg'
-];
+function loadSkybox(){
+	var urls = [
+	  'img/box_right1.jpg',
+	  'img/box_left2.jpg',
+	  'img/box_top3.jpg',
+	  'img/box_bottom4.jpg',
+	  'img/box_front5.jpg',
+	  'img/box_back6.jpg'
+	];
 
-var cubemap = THREE.ImageUtils.loadTextureCube(urls); // load textures
-cubemap.format = THREE.RGBFormat;
+	var cubemap = THREE.ImageUtils.loadTextureCube(urls); // load textures
+	cubemap.format = THREE.RGBFormat;
 
-var shader = THREE.ShaderLib['cube']; // init cube shader from built-in lib
-shader.uniforms['tCube'].value = cubemap; // apply textures to shader
+	var shader = THREE.ShaderLib['cube']; // init cube shader from built-in lib
+	shader.uniforms['tCube'].value = cubemap; // apply textures to shader
 
-// create shader material
-var skyBoxMaterial = new THREE.ShaderMaterial( {
-  fragmentShader: shader.fragmentShader,
-  vertexShader: shader.vertexShader,
-  uniforms: shader.uniforms,
-  depthWrite: false,
-  side: THREE.BackSide
-});
+	// create shader material
+	var skyBoxMaterial = new THREE.ShaderMaterial( {
+	  fragmentShader: shader.fragmentShader,
+	  vertexShader: shader.vertexShader,
+	  uniforms: shader.uniforms,
+	  depthWrite: false,
+	  side: THREE.BackSide
+	});
 
-// create skybox mesh
-var skybox = new THREE.Mesh(
-  new THREE.CubeGeometry(1000, 1000, 1000),
-  skyBoxMaterial
-);
-skybox.renderOrder = -1;
-scene.add(skybox);
+	// create skybox mesh
+	skybox = new THREE.Mesh(
+	  new THREE.CubeGeometry(1000, 1000, 1000),
+	  skyBoxMaterial
+	);
+	skybox.renderOrder = -1;
+	scene.add(skybox);
 
+}
+
+if(!isMobile.any()){
+	loadSkybox();
+}
 
 /* var label = document.createElement( 'p' );
 label.className = 'playerTag';
@@ -457,6 +464,6 @@ function onGenerateAsteroids(data){
 	});
 	
 }
-if( isMobile.any() ) alert('Mobile');
+
 socket.emit("generate asteroids");
 render();
